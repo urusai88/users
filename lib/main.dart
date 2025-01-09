4cea9b71e89e2ffe8e69dafda8ee826873ff1683
urusai <urusaich@gmail.com>
2025-01-09 17:23:17 +0400

    fix import

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tio/tio.dart';
-import 'package:users/src/data/entity/user.dart';
 
+import 'src/data.dart';
 import 'src/presentation.dart';
 import 'src/services.dart';
 
