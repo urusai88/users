12727734f1ecb065c16413a6ed9634ff056ff059
urusai <urusaich@gmail.com>
2025-01-12 23:35:52 +0400

    remove import

--- lib/src/presentation/screens/users/users.dart
+++ lib/src/presentation/screens/users/users.dart
@@ -2,8 +2,6 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:users/main.dart';
-import 'package:users/src/presentation/notifiers/brightness.dart';
 
 import '../../../domain.dart';
 import '../../../presentation.dart';
